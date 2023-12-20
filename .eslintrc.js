c2c4beaad9615ca129f9bd8829740896a5b25f41
akifo <akiho@bb07.net>
2023-12-20 11:24:34 +0900

    install @vue/eslint-config-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
-  extends: ["plugin:vue/vue3-recommended"],
-  rules: {},
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-typescript/recommended",
+  ],
+  rules: {
+    "no-console": "error",
+    "vue/no-unused-vars": "error",
+  },
 };
